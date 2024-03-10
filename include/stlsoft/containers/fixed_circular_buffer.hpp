8d836901f322c50c12a5a25994ba4805e5b18aaf
Matt Wilson <matthew@synesis.com.au>
2024-03-11 10:28:49 +1100

    `fixed_circular_buffer.hpp<>` : + more functionality

--- include/stlsoft/containers/fixed_circular_buffer.hpp
+++ include/stlsoft/containers/fixed_circular_buffer.hpp
@@ -6,6 +6,8 @@
 #include <stlsoft/stlsoft.h>
 
 #include <array>
+#include <limits>
+#include <stdexcept>
 
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -37,13 +39,31 @@ public: // types
     ,   V_capacity
     >                                                       class_type;
     typedef ss_size_t                                       size_type;
+    typedef ss_size_t                                       index_type;
+    typedef value_type&                                     reference;
+    typedef value_type const&                               const_reference;
 
     class iterator
     {
     private: // types
         friend class fixed_circular_buffer<T_value, V_capacity>;
+        typedef fixed_circular_buffer<T_value, V_capacity>      cb_type_;
     public:
         typedef iterator                                        class_type;
+
+    private: // construction
+        iterator(
+            cb_type_*   cb
+        ,   size_type   ix
+        )
+            : cb(cb)
+            , ix(ix)
+        {}
+    public:
+        iterator()
+            : cb(ss_nullptr_k)
+            , ix(std::numeric_limits<size_type>::max())
+        {}
 
     public:
         bool
@@ -51,10 +71,39 @@ public: // types
             class_type const& rhs
         ) const STLSOFT_NOEXCEPT
         {
-            ((void)&rhs);
+            if (ss_nullptr_k == cb)
+            {
+                STLSOFT_ASSERT(std::numeric_limits<size_type>::max() == ix);
 
-            return true;
+                if (ss_nullptr_k == rhs.cb)
+                {
+                    STLSOFT_ASSERT(std::numeric_limits<size_type>::max() == rhs.ix);
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (ss_nullptr_k == rhs.cb)
+                {
+                    STLSOFT_ASSERT(std::numeric_limits<size_type>::max() == rhs.ix);
+
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
         }
+
+    private:
+        cb_type_* const cb;
+        size_type       ix;
     };
 
     class const_iterator;
@@ -73,6 +122,18 @@ public: // construction
         : m_base_offset(0)
         , m_size(0)
     {}
+    fixed_circular_buffer(std::initializer_list<value_type> init)
+        : m_base_offset(0)
+        , m_size(0)
+    {
+        for (auto i = init.begin(); init.end() != i; ++i)
+        {
+            if (!try_push_back(*i))
+            {
+                STLSOFT_THROW_X(STLSOFT_NS_QUAL_STD(runtime_error)("`fixed_circular_buffer` too many"));
+            }
+        }
+    }
 
 
 public: // attributes
@@ -108,11 +169,103 @@ public: // attributes
     }
 
 
+public: // modifiers
+    bool
+    try_push_back(
+        value_type const& value
+    )
+    {
+        if (capacity() == size())
+        {
+            return false;
+        }
+        else
+        {
+
+            size_type const iix = translate_subscript_e2i_(m_base_offset + m_size);
+
+            m_elements[iix] = value;
+
+            ++m_size;
+
+
+            return true;
+        }
+    }
+
+
+public: // accessors
+    reference
+    at(index_type index)
+    {
+        check_range_(index);
+
+        return (*this)[index];
+    }
+
+    const_reference
+    at(index_type index) const
+    {
+        check_range_(index);
+
+        return (*this)[index];
+    }
+
+    reference
+    operator [](index_type ix) STLSOFT_NOEXCEPT
+    {
+        STLSOFT_MESSAGE_ASSERT("`fixed_circular_buffer` index out of range", (ix < m_size));
+
+        size_type const ix_internal = translate_subscript_e2i_(ix);
+
+        return m_elements[ix_internal];
+    }
+
+    const_reference
+    operator [](index_type ix) const STLSOFT_NOEXCEPT
+    {
+        STLSOFT_MESSAGE_ASSERT("`fixed_circular_buffer` index out of range", (ix < m_size));
+
+        size_type const ix_internal = translate_subscript_e2i_(ix);
+
+        return m_elements[ix_internal];
+    }
+
+    reference
+    front()
+    {
+        return at(0);
+    }
+
+    reference
+    back()
+    {
+        return at(size() - 1);
+    }
+
+    const_reference
+    front() const
+    {
+        return at(0);
+    }
+
+    const_reference
+    back() const
+    {
+        return at(size() - 1);
+    }
+
+
 public: // iteration
     iterator
     begin()
     {
-        return iterator();
+        if (empty())
+        {
+            return iterator();
+        }
+
+        return iterator(this, m_base_offset);
     }
 
     iterator
@@ -130,6 +283,35 @@ public: // iteration
     crbegin();
     const_reverse_iterator
     crend();
+
+
+private: // implementation
+    void
+    check_range_(index_type ix)
+    {
+#ifdef STLSOFT_CF_EXCEPTION_SUPPORT
+
+    if (!(ix < m_size))
+    {
+        STLSOFT_THROW_X(STLSOFT_NS_QUAL_STD(out_of_range)("`fixed_circular_buffer` index out of range"));
+    }
+#else
+
+    STLSOFT_SUPPRESS_UNUSED(ix);
+#endif /* STLSOFT_CF_EXCEPTION_SUPPORT */
+    }
+
+    size_type
+    translate_subscript_e2i_(
+        index_type eix
+    )
+    {
+        STLSOFT_ASSERT(eix < m_size);
+
+        size_type const iix = (eix + m_base_offset) % capacity();
+
+        return iix;
+    }
 
 
 private: // fields
--- test/unit/stlsoft/containers/test.unit.stlsoft.containers.fixed_circular_buffer/entry.cpp
+++ test/unit/stlsoft/containers/test.unit.stlsoft.containers.fixed_circular_buffer/entry.cpp
@@ -27,7 +27,7 @@
 #include <xtests/xtests.h>
 
 /* STLSoft header files */
-
+#include <stlsoft/stlsoft.h>
 
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -38,6 +38,9 @@ namespace
 {
 
     static void test_empty(void);
+    static void test_try_push_back(void);
+    static void test_fill(void);
+    static void test_initialiser_list(void);
 
 } // anonymous namespace
 
@@ -56,6 +59,9 @@ int main(int argc, char* argv[])
     if (XTESTS_START_RUNNER("test.unit.stlsoft.container.fixed_circular_buffer", verbosity))
     {
         XTESTS_RUN_CASE(test_empty);
+        XTESTS_RUN_CASE(test_try_push_back);
+        XTESTS_RUN_CASE(test_fill);
+        XTESTS_RUN_CASE(test_initialiser_list);
 
         XTESTS_PRINT_RESULTS();
 
@@ -90,7 +96,127 @@ static void test_empty(void)
 
         XTESTS_TEST(b.end() == b.begin());
     }
+}
 
+static void test_try_push_back(void)
+{
+    {
+        fcb_int_16_t   b;
+
+        XTESTS_TEST_BOOLEAN_TRUE(b.empty());
+        XTESTS_TEST_BOOLEAN_FALSE(b.full());
+
+        XTESTS_TEST_INTEGER_EQUAL(0, b.size());
+        XTESTS_TEST_INTEGER_EQUAL(16, b.capacity());
+
+        XTESTS_TEST(b.end() == b.begin());
+
+
+        bool r = b.try_push_back(1);
+
+        XTESTS_TEST_BOOLEAN_TRUE(r);
+
+        XTESTS_TEST_BOOLEAN_FALSE(b.empty());
+        XTESTS_TEST_BOOLEAN_FALSE(b.full());
+
+        XTESTS_TEST_INTEGER_EQUAL(1, b.size());
+        XTESTS_TEST_INTEGER_EQUAL(16, b.capacity());
+
+        XTESTS_TEST(b.end() != b.begin());
+
+        XTESTS_TEST_INTEGER_EQUAL(1, b.at(0));
+        XTESTS_TEST_INTEGER_EQUAL(1, b[0]);
+        XTESTS_TEST_INTEGER_EQUAL(1, b.back());
+        XTESTS_TEST_INTEGER_EQUAL(1, b.front());
+    }
+}
+
+static void test_fill(void)
+{
+    {
+        fcb_int_16_t   b;
+
+        XTESTS_TEST_BOOLEAN_TRUE(b.empty());
+        XTESTS_TEST_BOOLEAN_FALSE(b.full());
+
+        XTESTS_TEST_INTEGER_EQUAL(0, b.size());
+        XTESTS_TEST_INTEGER_EQUAL(16, b.capacity());
+
+        XTESTS_TEST(b.end() == b.begin());
+
+
+        for (int i = 1; i <= 16; ++i)
+        {
+            XTESTS_TEST_BOOLEAN_FALSE(b.full());
+
+            bool r = b.try_push_back(i);
+
+            XTESTS_TEST_BOOLEAN_TRUE(r);
+
+            XTESTS_TEST_BOOLEAN_FALSE(b.empty());
+
+            XTESTS_TEST_INTEGER_EQUAL(i, b.size());
+            XTESTS_TEST_INTEGER_EQUAL(16, b.capacity());
+
+            XTESTS_TEST(b.end() != b.begin());
+
+            XTESTS_TEST_INTEGER_EQUAL(i, b.at(i - 1));
+            XTESTS_TEST_INTEGER_EQUAL(i, b[i - 1]);
+            XTESTS_TEST_INTEGER_EQUAL(i, b.back());
+            XTESTS_TEST_INTEGER_EQUAL(1, b.front());
+        }
+
+        XTESTS_TEST_BOOLEAN_TRUE(b.full());
+    }
+}
+
+static void test_initialiser_list(void)
+{
+    {
+        fcb_int_16_t   b = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        XTESTS_TEST_BOOLEAN_FALSE(b.empty());
+        XTESTS_TEST_BOOLEAN_FALSE(b.full());
+
+        XTESTS_TEST_INTEGER_EQUAL(8, b.size());
+        XTESTS_TEST_INTEGER_EQUAL(16, b.capacity());
+
+        XTESTS_TEST(b.end() != b.begin());
+
+    }
+
+    {
+        fcb_int_16_t   b = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+        XTESTS_TEST_BOOLEAN_FALSE(b.empty());
+        XTESTS_TEST_BOOLEAN_TRUE(b.full());
+
+        XTESTS_TEST_INTEGER_EQUAL(16, b.size());
+        XTESTS_TEST_INTEGER_EQUAL(16, b.capacity());
+
+        XTESTS_TEST(b.end() != b.begin());
+
+        bool r = b.try_push_back(17);
+
+        XTESTS_TEST_BOOLEAN_FALSE(r);
+
+        XTESTS_TEST_INTEGER_EQUAL(16, b.size());
+    }
+
+    try
+    {
+        fcb_int_16_t   b = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+
+        XTESTS_TEST_FAIL("should not get here");
+    }
+    catch (std::runtime_error& /* x */)
+    {
+        // expected
+    }
+    catch (std::exception& x)
+    {
+        XTESTS_TEST_FAIL_WITH_QUALIFIER("should not get here", x.what());
+    }
 }
 
 
