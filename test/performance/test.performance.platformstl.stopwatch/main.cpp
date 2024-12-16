26becbc751b3645abe361c372c55d4bf3851956b
Matt Wilson <matthew@synesis.com.au>
2024-12-16 19:14:57 +1100

    **test.performance.platformstl.stopwatch** : + expanded scope of tests to measure `#pause()` / `#unpause()`

--- test/performance/test.performance.platformstl.stopwatch/main.cpp
+++ test/performance/test.performance.platformstl.stopwatch/main.cpp
@@ -4,7 +4,7 @@
  * Purpose: Perf-test for stopwatch types.
  *
  * Created: 15th March 2024
- * Updated: 1st December 2024
+ * Updated: 16th December 2024
  *
  * ////////////////////////////////////////////////////////////////////// */
 
@@ -30,10 +30,12 @@
 
 #include <platformstl/diagnostics/stopwatch.hpp>
 #include <platformstl/diagnostics/processtimes_stopwatch.hpp>
+#include <platformstl/synch/sleep_functions.h>
 #include <stlsoft/diagnostics/std_chrono_hrc_stopwatch.hpp>
 
 #include <iomanip>
 #include <iostream>
+#include <tuple>
 
 
 /* /////////////////////////////////////////////////////////////////////////
@@ -41,6 +43,7 @@
  */
 
 using stlsoft::ss_size_t;
+using stlsoft::ss_uint_t;
 typedef platformstl::stopwatch::interval_type               interval_t;
 
 
@@ -48,6 +51,13 @@ typedef platformstl::stopwatch::interval_type               interval_t;
  * functions
  */
 
+/// Evaluates cost of 1-start and N-stop calls
+///
+/// @tparam T_sw Type of stopwatch type to be evaluated
+///
+/// @param num_iterations Number of iterations
+///
+/// @return T.B.C.
 template<
     ss_typename_param_k T_sw
 >
@@ -77,7 +87,7 @@ test_start_stop_(
                 t.stop();
             }
 
-            anchoring_value += t.get_microseconds();
+            anchoring_value += t.get_seconds();
         }
 
         sw.stop();
@@ -88,6 +98,13 @@ test_start_stop_(
     return std::make_pair(interval, anchoring_value);
 }
 
+/// Evaluates cost of 1-start, 1-stop, N-pause, and N-unpause calls
+///
+/// @tparam T_sw Type of stopwatch type to be evaluated
+///
+/// @param num_iterations Number of iterations
+///
+/// @return T.B.C.
 template<
     ss_typename_param_k T_sw
 >
@@ -120,7 +137,7 @@ test_pause_unpause_(
 
             t.stop();
 
-            anchoring_value += t.get_microseconds();
+            anchoring_value += t.get_seconds();
         }
 
         sw.stop();
@@ -130,6 +147,100 @@ test_pause_unpause_(
 
     return std::make_pair(interval, anchoring_value);
 }
+
+/// Evaluates cost of 1-start, 1-stop, N-pause, and N-unpause calls
+///
+/// @tparam T_sw Type of stopwatch type to be evaluated
+///
+/// @param num_iterations Number of iterations
+/// @param delay_us Pause/unpause delay (in microseconds)
+///
+/// @return T.B.C.
+template<
+    ss_typename_param_k T_sw
+>
+std::tuple<
+    interval_t  // total_elapsed_time_ns
+,   interval_t  // total_measured_time_ns
+,   ss_size_t   // anchoring_value - this to incline the optimiser to not elide the whole thing
+>
+test_pause_unpause_around_delay_(
+    ss_size_t   num_iterations
+,   ss_uint_t   delay_us
+)
+{
+    platformstl::stopwatch  sw;
+    interval_t              total_elapsed_time_ns   =   0;
+    interval_t              total_measured_time_ns  =   0;
+    ss_size_t               anchoring_value         =   0;
+
+    for (int W = 2; W != 0; --W)
+    {
+        sw.start();
+
+        {
+            T_sw t;
+
+            t.start();
+
+            for (ss_size_t i = 0; i != num_iterations; ++i)
+            {
+                t.pause();
+
+                platformstl::micro_sleep(delay_us);
+
+                t.unpause();
+            }
+
+            t.stop();
+
+            anchoring_value += t.get_seconds();
+            total_measured_time_ns = t.get_nanoseconds();
+        }
+
+        sw.stop();
+
+        total_elapsed_time_ns = sw.get_nanoseconds();
+    }
+
+    return std::make_tuple(
+        total_elapsed_time_ns
+    ,   total_measured_time_ns
+    ,   anchoring_value
+    );
+}
+
+
+void
+display_results(
+    std::ostream&       stm
+,   ss_size_t           num_iterations
+,   char const*         type_name
+,   std::tuple<
+        interval_t  // total_elapsed_time_ns
+    ,   interval_t  // total_measured_time_ns
+    ,   ss_size_t   // anchoring_value - this to incline the optimiser to not elide the whole thing
+    > const&            r
+)
+{
+    stm
+        << '\t'
+        << type_name
+        << '\t'
+        << num_iterations
+        << '\t'
+        << std::setw(12) << std::right << std::get<0>(r)
+        << '\t'
+        << std::setw(12) << std::setfill(' ') << std::right << std::fixed << std::setprecision(3) << (static_cast<double>(std::get<0>(r)) / num_iterations)
+        << '\t'
+        << std::get<1>(r)
+        << '\t'
+        << std::setw(12) << std::setfill(' ') << std::right << std::fixed << std::setprecision(3) << (static_cast<double>(std::get<1>(r)) / num_iterations)
+        << '\t'
+        << std::get<2>(r)
+        << std::endl;
+}
+
 
 void
 display_results(
@@ -152,6 +263,8 @@ display_results(
         << '\t'
         << std::setw(12) << std::setfill(' ') << std::right << std::fixed << std::setprecision(3) << (static_cast<double>(r.first) / num_iterations)
         << '\t'
+        << '\t'
+        << '\t'
         << r.second
         << std::endl;
 }
@@ -166,6 +279,24 @@ int main(int /* argc */, char* /* argv */[])
     ss_size_t const NUM_ITERATIONS  =   1000000;
 
 #define DEFINE_TYPE_AND_NAME(t)                             char const* const type_name = #t; typedef t sw_t
+
+    std::cout
+        << '\t'
+        << "type"
+        << '\t'
+        << "#iterations"
+        << '\t'
+        << "elapsed (ns)"
+        << '\t'
+        << "elps/iter (ns)"
+        << '\t'
+        << "measured (ns)"
+        << '\t'
+        << "msrd/iter (ns)"
+        << '\t'
+        << "anchor value"
+        << std::endl;
+
 
     // start-stop
     {
@@ -232,6 +363,39 @@ int main(int /* argc */, char* /* argv */[])
             > const r = test_pause_unpause_<sw_t>(NUM_ITERATIONS);
 
             display_results(std::cout, NUM_ITERATIONS, type_name, r);
+        }
+    }
+
+    // pause-unpause (around delay)
+    {
+        std::cout << "`pause()` - `unpause()` (around delay; delay=1000us):" << std::endl;
+
+        ss_size_t const num_iterations  =   NUM_ITERATIONS / 10000;
+        ss_uint_t const delay_us        =   499 + 0;
+
+        {
+            DEFINE_TYPE_AND_NAME(platformstl::stopwatch);
+
+            std::tuple<
+                interval_t  // total_elapsed_time_ns
+            ,   interval_t  // total_measured_time_ns
+            ,   ss_size_t   // anchoring_value - this to incline the optimiser to not elide the whole thing
+            > const r = test_pause_unpause_around_delay_<sw_t>(num_iterations, delay_us);
+
+            display_results(std::cout, num_iterations, type_name, r);
+        }
+
+
+        {
+            DEFINE_TYPE_AND_NAME(stlsoft::std_chrono_hrc_stopwatch);
+
+            std::tuple<
+                interval_t  // total_elapsed_time_ns
+            ,   interval_t  // total_measured_time_ns
+            ,   ss_size_t   // anchoring_value - this to incline the optimiser to not elide the whole thing
+            > const r = test_pause_unpause_around_delay_<sw_t>(num_iterations, delay_us);
+
+            display_results(std::cout, num_iterations, type_name, r);
         }
     }
 
