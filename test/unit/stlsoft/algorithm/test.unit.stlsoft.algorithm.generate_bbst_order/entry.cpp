1b68e14569a28b30300a377e1fdf9fc69b45003b
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-03-20 12:09:16 +1100

    ~ moved **test.unit.stlsoft.algorithm.generate_binary** => **test.unit.stlsoft.algorithm.generate_bbst_order**

--- test/unit/stlsoft/algorithm/test.unit.stlsoft.algorithm.generate_bbst_order/entry.cpp
+++ test/unit/stlsoft/algorithm/test.unit.stlsoft.algorithm.generate_bbst_order/entry.cpp
@@ -1,10 +1,10 @@
 /* /////////////////////////////////////////////////////////////////////////
- * File:    test.unit.stlsoft.container.frequency_map/entry.cpp
+ * File:    test.unit.stlsoft.algorithm.generate_bbst_order/entry.cpp
  *
- * Purpose: Unit-tests for `stlsoft::frequency_map`.
+ * Purpose: Unit-tests for `stlsoft::generate_bbst_order`.
  *
  * Created: 19th March 2025
- * Updated: 19th March 2025
+ * Updated: 20th March 2025
  *
  * ////////////////////////////////////////////////////////////////////// */
 
@@ -17,7 +17,9 @@
  * test component header file include(s)
  */
 
-#include <stlsoft/containers/frequency_map.hpp>
+/*
+#include <stlsoft/algorithm/generate_bbst_order.hpp>
+ */
 
 
 /* ///////////////////////////////////////////////
@@ -29,6 +31,7 @@
 
 /* STLSoft header files */
 #include <stlsoft/meta/is_integral_type.hpp>
+#include <stlsoft/util/true_typedef.hpp>
 
 /* Standard C++ header files */
 #include <vector>
@@ -55,6 +58,7 @@ namespace
 {
 
     static void TEST_generate_binary_1();
+    static void TEST_generate_binary_2();
 } // anonymous namespace
 
 
@@ -69,9 +73,10 @@ int main(int argc, char *argv[])
 
     XTESTS_COMMANDLINE_PARSEVERBOSITY(argc, argv, &verbosity);
 
-    if (XTESTS_START_RUNNER("test.unit.stlsoft.container.frequency_map", verbosity))
+    if (XTESTS_START_RUNNER("test.unit.stlsoft.algorithm.generate_bbst_order", verbosity))
     {
         XTESTS_RUN_CASE(TEST_generate_binary_1);
+        XTESTS_RUN_CASE(TEST_generate_binary_2);
 
         XTESTS_PRINT_RESULTS();
 
@@ -100,6 +105,9 @@ namespace
 {
 
 static void TEST_generate_binary_1()
+{
+}
+static void TEST_generate_binary_2()
 {
 }
 } // anonymous namespace
