0d431fbb66fac7b9b1e62cb3ec6864d7c585972b
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-05-28 17:59:44 +1000

    ~ preparatory mods

--- include/stlsoft/internal/cccap/como.h
+++ include/stlsoft/internal/cccap/como.h
@@ -4,11 +4,11 @@
  * Purpose: Compiler feature discrimination for Comeau C/C++.
  *
  * Created: 7th February 2003
- * Updated: 25th November 2024
+ * Updated: 28th May 2025
  *
  * Home:    http://stlsoft.org/
  *
- * Copyright (c) 2019-2024, Matthew Wilson and Synesis Information Systems
+ * Copyright (c) 2019-2025, Matthew Wilson and Synesis Information Systems
  * Copyright (c) 2003-2019, Matthew Wilson and Synesis Software
  * All rights reserved.
  *
@@ -62,7 +62,7 @@
 # define STLSOFT_VER_H_STLSOFT_CCCAP_COMO_MAJOR     3
 # define STLSOFT_VER_H_STLSOFT_CCCAP_COMO_MINOR     18
 # define STLSOFT_VER_H_STLSOFT_CCCAP_COMO_REVISION  2
-# define STLSOFT_VER_H_STLSOFT_CCCAP_COMO_EDIT      81
+# define STLSOFT_VER_H_STLSOFT_CCCAP_COMO_EDIT      82
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 
@@ -166,7 +166,8 @@
  * which indicate that a given type is not used in the size-specific types.
  */
 
-#if defined(__LP64__)
+#if 0
+#elif defined(__LP64__)
 # define _STLSOFT_SIZEOF_CHAR                               (1)
 # define _STLSOFT_SIZEOF_SHORT                              (2)
 # define _STLSOFT_SIZEOF_INT                                (4)
