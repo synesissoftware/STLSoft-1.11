450240b788dc382d27a7b3afb4b9a2525232c8b9
Matt Wilson <matthew@synesis.com.au>
2025-05-30 20:13:18 +1000

    C++<20 compatibility

--- test/scratch/stlsoft/view/member_selector_view/main.cpp
+++ test/scratch/stlsoft/view/member_selector_view/main.cpp
@@ -6,16 +6,35 @@
 
 #include <time.h>
 
+
 int main()
 {
-    {
-        struct tm elements[] =
-        {
-            { .tm_mon = 1 },
-            { .tm_mon = 2 },
-            { .tm_mon = 3 },
-        };
 
+
+#if __cplusplus >= 202002L
+
+    struct tm elements[] =
+    {
+        { .tm_mon = 1 },
+        { .tm_mon = 2 },
+        { .tm_mon = 3 },
+    };
+#else
+
+    struct tm elements[3] =
+    {
+        {},
+        {},
+        {},
+    };
+    elements[0].tm_mon = 1;
+    elements[0].tm_mon = 2;
+    elements[0].tm_mon = 3;
+#endif
+
+
+
+    {
         auto const months = stlsoft::make_member_selector_view(elements, &tm::tm_mon);
 
         std::cout
@@ -30,13 +49,6 @@ int main()
     }
 
     {
-        struct tm elements[] =
-        {
-            { .tm_mon = 1 },
-            { .tm_mon = 2 },
-            { .tm_mon = 3 },
-        };
-
         auto const months = stlsoft::make_member_selector_view(std::begin(elements), std::end(elements), &tm::tm_mon);
 
         std::cout
@@ -51,13 +63,6 @@ int main()
     }
 
     {
-        struct tm elements[] =
-        {
-            { .tm_mon = 1 },
-            { .tm_mon = 2 },
-            { .tm_mon = 3 },
-        };
-
         std::list<struct tm> list_of_elements(std::begin(elements), std::end(elements));
 
         auto const months = stlsoft::make_member_selector_view(std::begin(list_of_elements), std::end(list_of_elements), &tm::tm_mon);
@@ -77,3 +82,4 @@ int main()
 
     return 0;
 }
+
