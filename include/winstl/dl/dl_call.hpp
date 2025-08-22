278bc649ba070265b03de6d4856f9a1daa117819
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-08-22 13:09:41 +1000

    Visual C++ 14 compatibility

--- include/winstl/dl/dl_call.hpp
+++ include/winstl/dl/dl_call.hpp
@@ -4,7 +4,7 @@
  * Purpose: Invocation of functions in dynamic libraries.
  *
  * Created: sometime in 1998
- * Updated: 20th March 2025
+ * Updated: 22nd August 2025
  *
  * Home:    http://stlsoft.org/
  *
@@ -53,8 +53,8 @@
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
 # define WINSTL_VER_WINSTL_DL_HPP_DL_CALL_MAJOR     2
 # define WINSTL_VER_WINSTL_DL_HPP_DL_CALL_MINOR     8
-# define WINSTL_VER_WINSTL_DL_HPP_DL_CALL_REVISION  10
-# define WINSTL_VER_WINSTL_DL_HPP_DL_CALL_EDIT      78
+# define WINSTL_VER_WINSTL_DL_HPP_DL_CALL_REVISION  11
+# define WINSTL_VER_WINSTL_DL_HPP_DL_CALL_EDIT      79
 #endif /* !STLSOFT_DOCUMENTATION_SKIP_SECTION */
 
 
@@ -253,6 +253,7 @@ public:
     typedef invalid_calling_convention_exception            class_type;
 private:
     typedef parent_class_type::string_type                  string_type;
+    typedef ws_size_t                                       size_type;
 public:
     /// The status code type
     typedef parent_class_type::status_code_type             status_code_type;
@@ -267,8 +268,8 @@ public:
     /// Constructs an instance of the exception based on the given
     /// function name, and Windows error code.
     ss_explicit_k
-    invalid_calling_convention_exception(char const* callingConventionSpecifier)
-        : parent_class_type(class_type::create_reason_(callingConventionSpecifier), ERROR_INVALID_FUNCTION)
+    invalid_calling_convention_exception(stlsoft::basic_string_view<char> callingConventionSpecifier)
+        : parent_class_type(class_type::create_reason_(callingConventionSpecifier.data(), callingConventionSpecifier.size()), ERROR_INVALID_FUNCTION)
         , m_ccs(callingConventionSpecifier)
     {}
 #ifndef STLSOFT_DOCUMENTATION_SKIP_SECTION
@@ -293,9 +294,12 @@ public:
 private:
     static
     string_type
-    create_reason_(char const* callingConventionSpecifier)
+    create_reason_(
+        char const* callingConventionSpecifier
+    ,   size_type   cchCallingConventionSpecifier
+    )
     {
-        return "Unrecognised or unsupported calling convention \"" + string_type(callingConventionSpecifier) + '"';
+        return "Unrecognised or unsupported calling convention \"" + string_type(callingConventionSpecifier, cchCallingConventionSpecifier) + '"';
     }
 /// @}
 
@@ -704,7 +708,7 @@ dl_determine_calling_convention_(C const*& functionName)
         } else
 # endif // STLSOFT_CF_STDCALL_SUPPORTED
         {
-            STLSOFT_THROW_X(invalid_calling_convention_exception(s0.c_str()));
+            STLSOFT_THROW_X(invalid_calling_convention_exception(s0));
         }
 
         functionName = s1.base();
